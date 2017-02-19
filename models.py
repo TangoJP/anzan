6cda7eb2e59a5740ed88c690f05e12378a2802b9
TangoJP <ryomarcher@gmail.com>
2017-02-19 09:46:18 -0500

    2nd commit

--- models.py
+++ models.py
@@ -33,28 +33,36 @@ class AddQuestion():
         else:
             print("Error: Level has to be and int between 1 & 5")
 
-class SubtractionQuestion():
+class SubtractQuestion():
 
     def __init__(self, level):
         if level == 1:
-            self.int1 = random.randint(1, 9)
-            self.int2 = random.randint(1, 9)
-            self.ans = self.int1 + self.int2
+            num1 = random.randint(1, 9)
+            num2 = random.randint(1, 9)
+            self.int1 = max(num1, num2)
+            self.int2 = min(num1, num2)
+            self.ans = self.int1 - self.int2
         elif level == 2:
-            self.int1 = random.randint(10, 49)
-            self.int2 = random.randint(10, 49)
-            self.ans = self.int1 + self.int2
+            self.int1 = random.randint(10, 99)
+            self.int2 = random.randint(1, 9)
+            self.ans = self.int1 - self.int2
         elif level == 3:
-            self.int1 = random.randint(10, 99)
-            self.int2 = random.randint(10, 99)
-            self.ans = self.int1 + self.int2
+            num1 = random.randint(10, 99)
+            num2 = random.randint(10, 99)
+            self.int1 = max(num1, num2)
+            self.int2 = min(num1, num2)
+            self.ans = self.int1 - self.int2
         elif level == 4:
-            self.int1 = random.randint(10, 99)
-            self.int2 = random.randint(100, 999)
-            self.ans = self.int1 + self.int2
+            num1 = random.randint(100, 999)
+            num2 = random.randint(10, 99)
+            self.int1 = max(num1, num2)
+            self.int2 = min(num1, num2)
+            self.ans = self.int1 - self.int2
         elif level == 5:
-            self.int1 = random.randint(100, 999)
-            self.int2 = random.randint(100, 999)
-            self.ans = self.int1 + self.int2
+            num1 = random.randint(100, 999)
+            num2 = random.randint(100, 999)
+            self.int1 = max(num1, num2)
+            self.int2 = min(num1, num2)
+            self.ans = self.int1 - self.int2
         else:
             print("Error: Level has to be and int between 1 & 5")
